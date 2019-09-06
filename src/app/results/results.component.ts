451f77395f65d6bfbfa7fff6a563bf915d5791a8
gegorov <gegorov@yahoo.com>
2019-09-06 18:04:01 +0300

    add results component

---
+++ src/app/results/results.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-results',
+  templateUrl: './results.component.html',
+  styleUrls: ['./results.component.css']
+})
+export class ResultsComponent implements OnInit {
+  constructor() {}
+
+  @Input() searchResult!: string;
+  ngOnInit() {}
+}
