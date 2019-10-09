ce847daed3f623c1449e36962e8a0c72fa7bed83
gegorov <gegorov@yahoo.com>
2019-10-09 15:26:16 +0300

    rename FilmPage component to FilmsToWatch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,10 @@ import { SearchPageModule } from './search-page/search-page.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
-import { FilmPageComponent } from './film-page/film-page.component';
+import { FilmsToWatchComponent } from './films-to-watch/films-to-watch.component';
 
 @NgModule({
-    declarations: [AppComponent, FilmPageComponent],
+    declarations: [AppComponent, FilmsToWatchComponent],
     imports: [BrowserAnimationsModule, BrowserModule, CoreModule, SearchPageModule, SharedModule],
     providers: [],
     bootstrap: [AppComponent]
---
+++ src/app/films-to-watch/films-to-watch.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-films-to-watch',
+    templateUrl: './films-to-watch.component.html',
+    styleUrls: ['./films-to-watch.component.scss']
+})
+export class FilmsToWatchComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
