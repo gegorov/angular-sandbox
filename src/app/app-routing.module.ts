00c63360c54aafc5ec40b4272d6a590ea0830c15
gegorov <gegorov@yahoo.com>
2019-10-09 17:52:38 +0300

    add routing and navigation to sidenave drawer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,1 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { FilmsToWatchComponent } from './films-to-watch/films-to-watch.component';
+
+const routes: Routes = [
+    { path: '', component: SearchPageComponent },
+    { path: 'filmstowatch', component: FilmsToWatchComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from './core/core.module';
 import { SearchPageModule } from './search-page/search-page.module';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { FilmsToWatchComponent } from './films-to-watch/films-to-watch.component';
-
 @NgModule({
     declarations: [AppComponent, FilmsToWatchComponent],
-    imports: [BrowserAnimationsModule, BrowserModule, CoreModule, SearchPageModule, SharedModule],
+    imports: [BrowserAnimationsModule, BrowserModule, AppRoutingModule, CoreModule, SearchPageModule, SharedModule],
     providers: [],
     bootstrap: [AppComponent]
 })
